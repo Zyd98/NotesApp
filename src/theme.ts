acb870af3bcbcfa3bb070d36f073eab6f9657bdf
Zyden <haoxian9838@gmail.com>
2024-11-19 03:32:08 +0800

    change theme pink to hotter pink

--- src/theme.ts
+++ src/theme.ts
@@ -2,7 +2,7 @@ const Colors = {
   primaryBackground: '#1e014d',
   secondaryBackground: '#2A1A5E',
   optionBackground: '#3B2C7A',
-  accent: '#ff6be6',
+  accent: '#ff4d94',
   textPrimary: 'white',
   borderColor: 'lightgray',
 };
